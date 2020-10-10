bbdccc7900fdbefdc86dd8d1acac49e7436704ea
orion-miller <33074666+orion-miller@users.noreply.github.com>
2020-10-10 17:29:47 -0400

    set axes props, make legend

--- IncomeExpensePlot.m
+++ IncomeExpensePlot.m
@@ -1,13 +1,5 @@
 function IncomeExpensePlot(app)
 
-Months = 1:1:12;
-
-cla(app.Balance_Axes)
-hold(app.Balance_Axes, 'on')
-
-plot(app.Balance_Axes, app.Data.(app.YearName)(1:12).Balances.WellsFargo)
-
-
 
 
 end
