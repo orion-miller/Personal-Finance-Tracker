57b7972cc6287db92e84710130cd1edbe4e25de4
orion-miller <33074666+orion-miller@users.noreply.github.com>
2020-10-09 20:40:01 -0400

    add income and expense catogries

--- Create_Data_Structure.m
+++ Create_Data_Structure.m
@@ -17,10 +17,23 @@ for iMonth = 1:12
     Data.Y2020(iMonth).Notes = {''};
     
     Data.Y2020(iMonth).Balances.WellsFargo = [];
-    Data.Y2020(iMonth).Balances.RobinHood = [];    
+    Data.Y2020(iMonth).Balances.RobinHood = [];
+    Data.Y2020(iMonth).Balances.Assets = [];    
     Data.Y2020(iMonth).Balances.FinnCU = []; 
     Data.Y2020(iMonth).Balances.PayPal = [];     
-    Data.Y2020(iMonth).Balances.NSLSC = [];     
+    Data.Y2020(iMonth).Balances.NSLSC = [];    
+    
+    Data.Y2020(iMonth).Incomes.Job = [];    
+    Data.Y2020(iMonth).Incomes.Other = [];     
+      
+    Data.Y2020(iMonth).Expenses.Bills = []; %Rent plus any other bills
+    Data.Y2020(iMonth).Expenses.Food = [];
+    Data.Y2020(iMonth).Expenses.Car = [];      
+    Data.Y2020(iMonth).Expenses.Transfers = [];    
+    Data.Y2020(iMonth).Expenses.WorkTravel = [];     
+    Data.Y2020(iMonth).Expenses.Other = [];    
+    
+    
     
 end
 
