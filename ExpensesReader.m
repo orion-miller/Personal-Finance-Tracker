9cd9537871dd54ca41e55b810637ec4853772f36
orion-miller <33074666+orion-miller@users.noreply.github.com>
2020-10-10 11:13:59 -0400

    start line loops, add in csv sheets

--- ExpensesReader.m
+++ ExpensesReader.m
@@ -1,24 +1,22 @@
 function EXP = ExpensesReader(app)
 
-[FileNames, FilePath] = uigetfile({'*.xlsx'},'Select Expense Spreadsheets','MultiSelect','on');
+cd('D:\!Orion_Programs\!Source_Controlled\Finances\Expense_Sheets')
+[FileNames, FilePath] = uigetfile('*.csv','Select Expense Spreadsheets','MultiSelect','on');
+% cd(FilePath)
 
+EXP = [];
 
+for iFiles = 1:length(FileNames)
 
+TempTable = readtable(FileNames{iFiles});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for iLines = length(TempTable)
+    
+    
+    
+    
+    
+end
 
 
 
